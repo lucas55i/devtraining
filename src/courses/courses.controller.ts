23b7efbbf1fcd7453b4ced8398f8ace5a5901146
Lucas Silva <lucas.devj@outlook.com>
2023-09-05 22:45:20 -0300

    Criação do controller.

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
+
+@Controller('courses')
+export class CoursesController {
+
+    @Get()
+    findAll() {
+        return "Listagem de cursos"
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return `Curso #${id}`
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.NO_CONTENT)
+    create(@Body('name') body) {
+        return body
+    }
+}
