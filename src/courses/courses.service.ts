9e21697b294b26a251a6d5dcce07e32773770826
Lucas Silva <lucas.devj@outlook.com>
2023-09-06 11:14:54 -0300

    Criação de metodos em services.

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { Course } from './entities/course.entity';
 
 @Injectable()
-export class CoursesService {}
+export class CoursesService {
+
+    private courses: Course[] = [
+        {
+            id: 1,
+            name: "Fundamentos do NestJs",
+            descripition: "Fundamentos do NestJs",
+            tasgs: ["node", "nest"]
+        }
+    ]
+
+    findAll() {
+        return this.courses
+    }
+
+    findOne(id: string) {
+        return this.courses.find((couser) => couser.id === Number(id));
+    }
+
+    create(createCourseDto: any) {
+        this.courses.push(createCourseDto)
+    }
+
+    update(id: string, updateCourseDto: any) {
+        const indexCourse = this.courses.findIndex(course => course.id === Number(id))
+
+        this.courses[indexCourse] = updateCourseDto;
+    }
+
+    remove(id: string) {
+        const indexCourse = this.courses.findIndex(course => course.id === Number(id))
+        if (indexCourse >= 0) {
+            this.courses.splice(indexCourse, 1)
+        }
+    }
+
+}
