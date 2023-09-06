1e2d80c7872fe1685addbbe713ae5854c95d9f9f
Lucas Silva <lucas.devj@outlook.com>
2023-09-06 17:25:17 -0300

    Implementa Dto nos metodos de create e update.

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { CoursesService } from './courses.service';
+import { CreateCourseDto } from './dto/create-course.dto';
+import { UpdateCourseDto } from './dto/update-course.dto';
 
 @Controller('courses')
 export class CoursesController {
@@ -16,15 +18,14 @@ export class CoursesController {
     }
 
     @Post()
-    create(@Body() body) {
-        return this.couserService.create(body)
+    create(@Body() createCourseDto: CreateCourseDto) {
+        return this.couserService.create(createCourseDto)
     }
 
     @Patch(':id')
-    update(@Param('id') id: string, @Body() body) {
-        return this.couserService.update(id, body)
+    update(@Param('id') id: string, @Body() updateCourseDto: UpdateCourseDto) {
+        return this.couserService.update(id, updateCourseDto)
     }
-
 
     @Delete(':id')
     remove(@Param('id') id: string) {
