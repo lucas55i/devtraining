57aad9cd54a6af02197af536da8ec3137e0405f1
Lucas Jesus <lucas.devj@outlook.com>
2023-09-10 14:39:03 -0300

    Adiciona o TagEntity como forFeature.

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -3,9 +3,10 @@ import { CoursesController } from './courses.controller';
 import { CoursesService } from './courses.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Course } from './entities/course.entity';
+import { TagEntity } from './entities/tag.enity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Course])],
+  imports: [TypeOrmModule.forFeature([Course, TagEntity])],
   controllers: [CoursesController],
   providers: [CoursesService],
 })
