e486313675bfa7c7c542e1324f2888854ae17a33
Lucas Jesus <lucas.devj@outlook.com>
2023-09-24 18:30:09 -0300

    Cria o courses providers.

---
+++ src/courses/courses.providers.ts
@@ -0,0 +1,15 @@
+import { DataSource } from 'typeorm';
+import { Course } from './entities/course.entity';
+
+export const coursesProviders = [
+    {
+        provide: 'COURSES_REPOSITORY',
+        useFactory: (dataSource: DataSource) => dataSource.getRepository(Course),
+        inject: ['DATA_SOURCE'],
+    },
+    {
+        provide: 'TAGS_REPOSITORY',
+        useFactory: (dataSource: DataSource) => dataSource.getRepository(Course),
+        inject: ['DATA_SOURCE'],
+    },
+];
